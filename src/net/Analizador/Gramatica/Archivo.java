9fdd664bcaa52496aea383dc3476fdbbcede2b46
Miguel Rodriguez <angek37@outlook.com>
2017-04-20 14:19:00 -0500

    Agrega package nuevos para trabajar

---
+++ src/net/Analizador/Gramatica/Archivo.java
@@ -0,0 +1,14 @@
+package net.Analizador.Gramatica;
+
+import java.io.*;
+
+public class Archivo {
+	public Archivo(){
+		String name = "gramatica.txt";
+		leer(name);
+	}
+	
+	public void leer(String name){
+		
+	}
+}
