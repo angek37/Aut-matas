058072180f50070420bdc09cb1e29e73a2593b52
Miguel Rodriguez <angek37@outlook.com>
2017-04-21 21:42:31 -0500

    Clasifica en otra estructura los simbolos terminales

--- src/net/Analizador/Gramatica/Gramatica.java
+++ src/net/Analizador/Gramatica/Gramatica.java
@@ -2,15 +2,79 @@ package net.Analizador.Gramatica;
 
 public class Gramatica {
 	private char del = '>';
-	private String[] term = new String[1];
+	private String[] term = new String[1]; // Almacena los simbolos terminales
 	private int co = 0;
+	private String[][] nterm;
+	private int ca = 0;
 	
 	public Gramatica(){
 		Archivo a = new Archivo();
 		String[] g = a.getGramar();
 		Nterminales(g);
+		Terminales(g);
 	}
 	
+	public void Terminales(String[] g){
+		String aux;
+		boolean fl, fl2;
+		int c = 0;
+		int c2;
+		for(int x = 0; x < g.length; x++){
+			fl = false;
+			aux = "";
+			for(int y = 0; y < g[x].length(); y++){
+				if(g[x].charAt(y) == del){
+					fl = true;
+				}
+				if(fl && g[x].charAt(y) != del){
+					aux += g[x].charAt(y);
+				}
+			}
+			c2 = c;
+			if((c = WhoTerm(g, x)) != c2){
+				ca = 0;
+			}
+			InTerminales(aux, c);
+		}
+		System.out.println("\n\nTerminales: ");
+		PrintBArray(nterm);
+	}
+
+	public void InTerminales(String a, int term){
+		String[] aux;
+		if(a != ""){
+			ca++;
+			aux = new String[ca];
+			for(int x = 0; x < nterm[term].length; x++){
+				aux[x] = nterm[term][x];
+			}
+			nterm[term] = new String[ca];
+			aux[ca-1] = a;
+			for(int x = 0; x < nterm[term].length; x++){
+				nterm[term][x] = aux[x];
+			}
+			aux = null;
+		}
+	}
+	
+	public int WhoTerm(String[] g , int a){
+		int pos = 0;
+		String aux = "";
+		int y = 0;
+		while(g[a].charAt(y) != del){
+			if(g[a].charAt(y) != ' '){
+				aux += g[a].charAt(y);
+			}
+			y++;
+		}
+		for(int x = 0; x < term.length; x++){
+			if(aux.equals(term[x])){
+				pos = x;
+			}
+		}
+		return pos;
+	}
+
 	public void Nterminales(String[] g){
 		String aux = "";
 		int y;
@@ -26,8 +90,12 @@ public class Gramatica {
 			aux = "";
 		}
 		
-		System.out.println("\n\nTerminales: ");
+		System.out.println("\n\nNO Terminales: ");
 		PrintArray(term);
+		nterm = new String[term.length][];
+		for(int x = 0; x < term.length; x++){
+			nterm[x] = new String[1];
+		}
 	}
 	
 	public void isRepeat(String a){
@@ -57,6 +125,15 @@ public class Gramatica {
 	public void PrintArray(String[] a){
 		for(int x = 0; x < a.length; x++){
 			System.out.println(a[x]);
+		}
+	}
+	
+	public void PrintBArray(String[][] a){
+		for(int x = 0; x < a.length; x++){
+			for(int y = 0; y < a[x].length; y++){
+				System.out.print(a[x][y] + " ");
+			}
+			System.out.println("\n");
 		}
 	}
 	
